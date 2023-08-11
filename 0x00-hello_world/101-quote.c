27f55e0dd49455cda0bf491a44660be027e52d63
Ibrahim_Adel <hima96326@gmail.com>
2023-08-10 19:37:20 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,8 +11,7 @@
 */
 
 int main(void)
-{	char first_arr[] =
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{	char first_arr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, first_arr, 59);
 	return (1);
