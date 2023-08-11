b72cbe3766196c395ac6a86896c278e5fbd41b4d
Ibrahim_Adel <hima96326@gmail.com>
2023-08-10 19:39:49 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio,h>
 #include <unistd.h>
 
 /**
@@ -7,12 +6,12 @@
  * Description: a C program prints by using
  * write(start,const array, the size of the array)
  *
- * Return: always 1 (not success)
+ * Return: 1 (not success)
 */
 
 int main(void)
-{	char first_arr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{	char arr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, first_arr, 59);
+	write(1, arr, 59);
 	return (1);
 }
