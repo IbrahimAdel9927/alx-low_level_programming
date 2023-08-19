ba13029dcbe5f4ff64e97e3298a64acde65e2c92
Ibrahim_Adel <hima96326@gmail.com>
2023-08-19 04:32:13 -0700

    done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,7 +15,7 @@ void print_times_table(int n)
 		for (i = 1; i <= n; i++)
 		{
 			_putchar(48);
-			for (j = 0; i <= n; i++)
+			for (j = 0; j <= n; i++)
 			{
 				_putchar(',');
 				_putchar(' ');
