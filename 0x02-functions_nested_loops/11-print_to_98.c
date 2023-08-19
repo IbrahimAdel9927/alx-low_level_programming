a6f3d8ff935de54906e9db8b4a0e306d94d7f20f
Ibrahim_Adel <hima96326@gmail.com>
2023-08-19 03:51:40 -0700

    done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,8 +15,11 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			_putchar(i + 48);
-			_putchar(',');
-			_putchar(' ');
+			if (i == 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
 	else
@@ -24,8 +27,11 @@ void print_to_98(int n)
 		for (i = n; i >= 98; i--)
 		{
 			_putchar(i + 48);
-			_putchar(',');
-			_putchar(' ');
+			if (i == 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
 	_putchar('\n');
