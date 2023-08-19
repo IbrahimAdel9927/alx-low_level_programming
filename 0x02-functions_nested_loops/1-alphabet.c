6cc25beda69df2f7e9fd459cf3ec7ca9afd93127
Ibrahim_Adel <hima96326@gmail.com>
2023-08-19 01:11:57 -0700

    done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: a C programe use a functions such as void print_alphabet(void).
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	void print_alphabet(void);
+	return (0);
+}
