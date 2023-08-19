bda136f382922f68dd894c5f296a757f9204455d
Ibrahim_Adel <hima96326@gmail.com>
2023-08-19 02:38:00 -0700

    done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,9 +14,12 @@ int print_last_digit(int n)
 
 	if (n < 0)
 	{
-		n = n * -1;
+		last = (n % 10) * -1;
 	}
-	last = n % 10;
+	else
+	{
+		last = n % 10;
+	}
 
 	_putchar(last + 48);
 	return (last);
