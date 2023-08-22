9a783d8127e4afe0f9f52297aca3b25c20ba7a71
Ibrahim_Adel <hima96326@gmail.com>
2023-08-21 17:31:26 -0700

    done

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Descreption: a program that generates random valid passwords
+ * for the program 101-crackme.
+ *
+ * Return:  0 (success)
+*/
+
+int main(void)
+{
+	int a;
+	char b;
+
+	srand(time(NULL));
+	while (a <= 2645)
+	{
+		b = rand() % 120;
+		a += b;
+		putchar(b);
+	}
+	putchar(2772 - a);
+	return (0);
+}
