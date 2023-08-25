2a531e385f6cbab3f14adc6c2ab5875020c60caa
Ibrahim_Adel <hima96326@gmail.com>
2023-08-25 11:33:19 -0700

    done

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - a function that prints an integer.
+ *
+ * @n: the input
+*/
+
+void print_number(int n)
+{
+	int i;
+
+	i = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -n;
+	}
+
+	if (i / 10 != 0)
+	{
+		print_number(i / 10);
+	}
+	_putchar((i % 10) + '0');
+}
