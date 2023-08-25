119b46dd613f77271ec398315e72613c838a5b09
Ibrahim_Adel <hima96326@gmail.com>
2023-08-25 12:44:51 -0700

    done

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -24,8 +24,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	else
 		l = g;
 	if (l + 1 > size_r)
-		return (0);
-	r[l] = '\0';
+		r[l] = '\0';
 	for (k = l - 1 ; k >= 0 ; k--)
 	{
 		i--;
