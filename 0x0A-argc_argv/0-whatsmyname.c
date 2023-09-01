5b9e3b1b67f86bc6580981f2a1fd0f4f1a54ba6c
Ibrahim_Adel <hima96326@gmail.com>
2023-09-01 15:55:39 -0700

    done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: always 0
+*/
+
+int main(int a, char const *b)
+{
+	(void)a;
+	printf("%s\n", b[0]);
+	return (0);
+}
