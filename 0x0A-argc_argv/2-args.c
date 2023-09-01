c82f24ae647c6d9ee15afa97d7e22aed9eb00fdf
Ibrahim_Adel <hima96326@gmail.com>
2023-09-01 16:20:00 -0700

    done

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @a: first input
+ * @b: second input
+ *
+ * Return: always 0
+*/
+
+int main(int a, char *b[] __attribute__((unused)))
+{
+	int i = 0;
+	while (a--)
+	{
+		printf("%s\n", b[i]);
+		i++;
+	}
+	return (0);
+}
