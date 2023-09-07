27636ab95d374659acff7de1031b068442e47fe8
Ibrahim_Adel <hima96326@gmail.com>
2023-09-07 15:48:22 -0700

    done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,20 +18,30 @@ char *string_nconcat(char *a, char *b, unsigned int n)
 	unsigned int i, j, c, d;
 
 	if (a == NULL)
+	{
 		a = "";
+	}
 	if (b == NULL)
+	{
 		b = "";
+	}
 	for (c = 0; a[c] != '\0'; c++)
 		;
 	for (d = 0; b[d] != '\0'; d++)
 		;
 	x = malloc(c + n + 1);
 	if (x == NULL)
+	{
 		return (NULL);
+	}
 	for (i = 0; a[i] != '\0'; i++)
+	{
 		x[i] = a[i];
+	}
 	for (j = 0; b[j] != '\0'; j++, i++)
+	{
 		x[i] = b[j];
+	}
 	x[i] = '\0';
 	return (x);
 }
