e5464144f7d24c911fb4c5134c3061f226703483
Ibrahim_Adel <hima96326@gmail.com>
2023-10-02 17:36:13 -0700

    done

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _sl - return the length
+ *
+ * @s: the input
+ *
+ * Return: int
+*/
+int _sl(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * create_file - a function that creates a file.
+ *
+ * @filename: first input
+ * @text_content: second input
+ *
+ * Return: int
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int f;
+	ssize_t bs = 0, ln = _sl(text_content);
+
+	if (!filename)
+		return (-1);
+	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (f == -1)
+		return (-1);
+	if (ln)
+		bs = write(f, text_content, ln);
+	close(f);
+	return (bs == ln ? 1 : -1);
+}
